edbd388b5325a39c890fcc5ccb17cd7b910be748
srlsrx <nicolasin97@gmail.com>
2025-01-28 18:59:53 +0100

    Fixed accordion issue

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,21 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
-
-    accordionHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            // Cerrar todos los acordeones abiertos
-            const activeHeader = document.querySelector('.accordion-header.active');
-            const activeContent = document.querySelector('.accordion-content.active');
-
-            if (activeHeader && activeHeader !== header) {
-                activeHeader.classList.remove('active');
-                activeContent.classList.remove('active');
-            }
-
-            // Alternar el estado del acordeón actual
-            header.classList.toggle('active');
-            header.nextElementSibling.classList.toggle('active');
-        });
-    });
-});
+const accordeon = document.getElementsByClassName('accordion-item');
+for(i=0; i<accordeon.length; i++){
+    accordeon[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+    
+};
 
